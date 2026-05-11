cd319a4104e0eed71d38e090dd2f743c17e0f054
onyo <daniluca091@gmail.com>
2026-05-11 22:19:53 +0300

    First commit

---
+++ onion.py
@@ -0,0 +1,19 @@
+import discord
+import os
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send('Hello World')
+
+bot.run(os.getenv('DISCORD_API_TOKEN'))
